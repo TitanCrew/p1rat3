46abc35b98c4674f670e0d68c92735021e14d7ca
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-01-08 21:58:42 +0530

    Update wappalyzer.py

--- tools/wappalyzer.py
+++ tools/wappalyzer.py
@@ -1,6 +1,7 @@
 import requests
 import json
-import os 
+import os
+
 
 class lookup:
     def __init__(self, api_key):
@@ -8,13 +9,9 @@ class lookup:
 
     def get_stack(self, url):
         to_fetch = f"https://api.wappalyzer.com/v2/lookup/?urls={url}&live=true&recursive=false"
-        headers = { "x-api-key": self.api_key }
+        headers = {"x-api-key": self.api_key}
 
-        # with open("data/scan/wappalyzer.json", "w") as file:
-        #     json.dump(requests.get(to_fetch, headers = headers).json(), file, indent = 4, separators = (",", ": "))
+        with open("data/scan/wappalyzer.json", "w") as file:
+            json.dump(requests.get(to_fetch, headers=headers).json(), file, indent=4, separators=(",", ": "))
 
         return json.loads(open("data/scan/wappalyzer.json", "r").read())
-
-if __name__ == "__main__":
-    app = lookup(os.getenv("wappalyzer_api"))
-    print(app.get_stack("https://tryhackme.com"))
