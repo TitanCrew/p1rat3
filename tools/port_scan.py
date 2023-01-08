32b846da48a8d3306c45f9f49fb401481baf5705
Sumit0673 <Sumit0673@github.com>
2023-01-08 07:47:11 -0500

    Added XSS scanner

--- tools/port_scan.py
+++ tools/port_scan.py
@@ -5,9 +5,11 @@ import os
 
 def rustscan(target):
     final_command = f"sudo rustscan -a {target} -- -Pn -O -sV -sC -oX data/scan/nmap.xml"
+    print(f'[+] RUNNING COMMAND {final_command}')
     subprocess.run(final_command, shell=True, stdout=subprocess.DEVNULL)
+    print(f'[+] PORT SCAN SUCCESSFUL')
 
     while not os.path.exists("data/scan/nmap.xml"):
         continue
-
+    print("[+] PORT DATA SAVED")
     return final_port_data()
